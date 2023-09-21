f6baaa3b74cf83821b27992ff0822b8408e2fff1
epj <e.jimenez@zertificon.com>
2023-09-21 11:04:08 +0200

    config: setting the colors and font in the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["League Spartan", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "primary-800": "#7C5DFA",
+        "primary-500": "#9277FF",
+        "secondary-900": "#1E2139",
+        "secondary-700": "#252945",
+        "tertiary-800": "#EC5757",
+        "tertiary-300": "#FF9797",
+        light: "#F8F8FB",
+        "dark-900": "#0C0E16",
+        "dark-800": "#131625",
+        "brand-blue-800": "#7E88C3",
+        "brand-blue-300": "#DFE3FA",
+        "brand-gray": "#888EB0",
+      },
+    },
   },
   plugins: [],
 };
